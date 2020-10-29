ca24593c5482672cb9f7c498c02044d9c2285def
Jon Butterworth <jon.butterworth@forgerock.com>
2020-10-29 15:48:15 +0000

    .gitignore fix

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import slack
+import random
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+from triggers import get_response_key
+from resp_map import resp_dict
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGN_SECRET'], '/slack/events', app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call("auth.test")['user_id']# clean this up - seems messy
+BOT_NAME = '<@' + BOT_ID + '>' # clean this up - seems messy
+
+def handle_command(command):
+    response_key = get_response_key(command, regex_type='match') or get_response_key(command, regex_type='search')
+    response = random.choice(resp_dict[response_key])
+
+    # if not (response_key or command):
+    #     response_key = 'no_command'
+    # elif not response_key:
+    #     response_key = 'search'
+    #     command = 'search' + command
+
+    if callable(response):
+        response = response(command)
+
+    return response
+
+
+@slack_event_adapter.on('message')
+def message(payload):
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user')
+    text = event.get('text')
+
+    if BOT_ID in text:
+        command = text.split(BOT_ID)[1].strip('>').lstrip().lower()
+        response = handle_command(command)
+
+        client.chat_postMessage(channel=channel_id, text=response)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ responses.py
@@ -0,0 +1,72 @@
+import random
+import os
+import requests
+from bs4 import BeautifulSoup as bs
+from textblob import Word
+from pyowm import OWM
+from pathlib import Path
+from dotenv import load_dotenv
+import json
+from requests import Request, Session
+
+import nltk
+nltk.download('wordnet')
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+def check_weather(command):
+    fluff_words = ['in', 'over', 'on', 'like']
+    command = command.replace("?", "").split('weather')[1].split()
+    command = ",".join(filter(lambda x: x not in fluff_words, command))
+
+    if len(command.strip()) <= 1:
+        command = 'Exeter'
+
+    owm = OWM(os.environ['PYOWM_KEY'])
+    w = owm.weather_manager().weather_at_place(command).weather
+    report = "It's {temp} degrees and I would describe the condition as {condition}.".format(
+        temp=w.temperature('celsius')["temp"], condition=w.detailed_status)
+    return report
+
+
+def define(command):
+    word = command.split('define')[1].split()
+    word = ", ".join(word)
+    return Word(word).definitions[0]
+
+
+def joke(_):
+    url = requests.get('http://theoatmeal.com/djtaf/')
+    soup = bs(url.content, 'lxml')
+    random_joke = soup.find(class_='part1').get_text()
+    random_answer = soup.find(id='part2_0').get_text()
+    return "\n".join([random_joke, random_answer])
+
+def crypto_coin_price(command):
+    crypto_key = os.environ['CRYPTO_KEY']
+
+    cryptos = ['bitcoin', 'litecoin', 'ethereum', 'xrp']
+    for x in command.replace("?", "").split():
+        if x in cryptos:
+            crypto = x
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'slug': crypto,
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': crypto_key
+    }
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    for x in data['data'].values():
+        price = x['quote']['USD']['price']
+
+    return f'The current price of {crypto} is ${price:.2f}'
